172a3188cfce0c80e4e2d89b4cac1d6b46f08f9a
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 11:29:01 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,22 +14,25 @@ int main(void)
 {
 	int i, j;
 
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
+	for (i = 0; i < 100; i++)
+	{
+		for (j = (i == 0 ? 1 : i); j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 
-            if (!(i == 99 && j == 99)) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (!(i == 99 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
