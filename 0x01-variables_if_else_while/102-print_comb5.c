a14b0e0d6911c0ff3e20e01f21c44fed91d8cf7b
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 12:04:44 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*more headers goes there */
+
+/*betty style doc for function main goes there */
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main()
 {
 	int i, j;
