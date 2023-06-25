978ea33ac9e70138c096f88dafc7b3fcf5350fd0
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-25 10:02:11 -0700

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,7 @@
  *
  *@ch: The character to be checked.
  *
- *Return 1 if the character is a digit, 0 otherwise.
+ *Return: 1 if the character is a digit, 0 otherwise.
  */
 int _isdigit(char ch)
 {
