2e3987a9fbf3bd93da87ae7363c562677e494211
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 21:17:16 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,6 @@ int _putchar(char c);
 void times_table(void)
 {
 	int i, j, result;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -20,11 +19,13 @@ void times_table(void)
 			
 			if (result < 10)
 			{
+				_putchar(' ');
 				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar(result + '0');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 
 			if (j < 9)
