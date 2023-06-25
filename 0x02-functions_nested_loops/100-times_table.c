5e729414a63963bea608498012470bd27228d141
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-25 00:25:09 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,77 +12,96 @@ int _putchar(char c);
  */
 void print_times_table(int n)
 {
-	int i, j;
+    int i, j;
+    int last_row = n;
+	
+	if (n < 0 || n > 15) {
+        return;
+    }
+	
+	if (n == 0) {
+	    _putchar(n + '0');
+	    _putchar('\n');
+        return;
+    }
 
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= n; j++) {
+            int result = i * j;
+ 
+            if (j > 0) {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');                
+                _putchar(' ');
 
-	if (n == 0)
-	{
-		_putchar(n + '0');
-		_putchar('\n');
-		return;
-	}
+            }else if (result >= 10 && result <= 99) {
+                _putchar(',');
+            }
+            else if (result >= 100)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			int result = i * j;
-
-			if (result < 10)
-			{
-				_putchar(result + '0');
-				if (i != n)
-				{
-					_putchar(',');
-				}
-				else
-				{
-					if (j != n) _putchar(',');
-				}
-
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (result >= 10 && result <= 99)
-			{
-				_putchar(result / 10 + '0');
+            if (result < 10) {
+                _putchar(result + '0');
+            }
+            else if (result >= 10 && result <= 99) {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+            else if (result >= 100)
+            {
+                _putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
 				_putchar(result % 10 + '0');
-				if (i != n)
+            }
+            else
+            {
+                
+            }
+
+           /* if (result < 10) {
+                _putchar(result + '0');
+                if (j != n || i !=last_row)
+    			{
+    				_putchar(',');
+    			}
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(' ');
+                
+            }
+            else if (result >= 10 && result <= 99) {
+                _putchar(result / 10 + '0');
+				_putchar(result % 10 + '0'); 
+                if (i =last_row)
 				{
 					_putchar(',');
 				}
-				else
-				{
-					if (j != n) _putchar(',');
-				}
-
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (result >= 100)
+                _putchar(' ');
+                _putchar(' ');
+            }
+            else if (result >= 100)
 			{
 				_putchar(result / 100 + '0');
 				_putchar((result / 10) % 10 + '0');
 				_putchar(result % 10 + '0');
-				if (i != n)
-				{
-					_putchar(',');
-				}
-				else
-				{
-					if (j != n) _putchar(',');
-				}
-
-				_putchar(' ');
+                if (i != n)
+    			{
+    				_putchar(',');
+    			}
+                _putchar(' ');
 			}
-			else {}
-		}
+            else{
+                
+            }*/
 
-		_putchar('\n');
-	}
+            
+        }
+
+        _putchar('\n');
+        //if(i!=n)_putchar(',');
+    }
 }
