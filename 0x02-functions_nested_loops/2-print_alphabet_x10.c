1f0400b32be7a927041ec97d2bd3b782753f4989
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 18:24:26 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,17 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
+		char letter = 'a';
 		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
+		_putchar('\n');
 	}
 
 	_putchar('\n');
