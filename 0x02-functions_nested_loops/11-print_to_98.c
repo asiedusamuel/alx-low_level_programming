5ae1a1d1611cd2e761b70c0fe4aef0fbd7f999b8
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 22:56:05 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,47 +4,79 @@
 int _putchar(char c);
 
 /**
- * add - Adds two integers and returns the result.
+ *print_to_98 - prints all natural numbers from n to 98.
  *
- * @n: The first integer to be added.
+ *@n: The first integer to be added.
  *
- * Return: void.
+ *Return: void.
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
-    {
-        for (; n < 98; n++)
-        {
-            _putchar(n / 10 + '0');
-            _putchar(n % 10 + '0');
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-    else
-    {
-        int i;
-        
-        for (i = n; i >= 98; i--) {
-            if (i > 99) {
-                _putchar(i / 100 + '0');
-                _putchar((i / 10) % 10 + '0');
-                _putchar(i % 10 + '0');
-            } else {
-                _putchar(i / 10 + '0');
-                _putchar(i % 10 + '0');
-            }
-    
-            if (i > 98) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
+	{
+		int i;
 
-    }
+		for (i = n; i <= 98; i++)
+		{
+			int num = i;
 
-    _putchar('9');
-    _putchar('8');
-    _putchar('\n');
+			if (num < 0)
+			{
+				_putchar('-');
+				num = -num;
+			}
+
+			if (num == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				int divisor = 1000000000;
+
+				while (divisor > num)
+					divisor /= 10;
+
+				while (divisor != 0)
+				{
+					_putchar((num / divisor) + '0');
+					num %= divisor;
+					divisor /= 10;
+				}
+			}
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 99)
+			{
+				_putchar(i / 100 + '0');
+				_putchar((i / 10) % 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+
+			if (i > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+
+	_putchar('\n');
 }
