549c18711a518630cdf9d9ceb6aef8e96c947c0f
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-25 00:55:14 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,56 +12,69 @@ int _putchar(char c);
  */
 void print_times_table(int n)
 {
-    int i, j;
-    if (n < 0 || n > 15) {
-        return;
-    }
-    
-    if (n == 0) {
-	    _putchar(n + '0');
-	    _putchar('\n');
-        return;
-    }
+	int i, j;
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
 
+	if (n == 0)
+	{
+		_putchar(n + '0');
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
-            int result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
 
-            if (j > 0) {
-                _putchar(','); 
+			if (j > 0)
+			{
+				_putchar(',');
 
-                if (result < 10) {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar(' ');
-                } else if (result <= 100) {
-                    _putchar(' ');
-                } else if (result > 100) {
-                    _putchar(' ');
-                }
-            }
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result <= 100)
+				{
+					_putchar(' ');
+				}
+				else if (result > 100)
+				{
+					_putchar(' ');
+				}
+			}
 
-            if (result < 10) {
-                if(i > 0 && (j > 0 && i> 9)){
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                
-                _putchar(result + '0');
-            } else if (result < 100) {
-                _putchar(' ');
-                _putchar(result / 10 + '0');
-                _putchar(result % 10 + '0');
-            } else {
-                _putchar(result / 100 + '0');
-                _putchar((result / 10) % 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-        }
+			if (result < 10)
+			{
+				if (i > 0 && (j > 0 && i > 9))
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 
-        _putchar('\n');
-    }
+				_putchar(result + '0');
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
 
+		_putchar('\n');
+	}
 }
-
