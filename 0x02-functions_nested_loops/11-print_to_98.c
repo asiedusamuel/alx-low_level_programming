72f54b7bc75050fd4fe74c36726c4d62fd4a6a2a
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-06-24 21:57:33 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,8 @@ void print_to_98(int n)
     {
         for (i = n; i <= 98; i++)
         {
-            _putchar(i + '0');
+            _putchar(i / 10 + '0');
+            _putchar(i % 10 + '0');
 
             if (i != 98)
             {
