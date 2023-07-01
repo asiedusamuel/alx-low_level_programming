ff5f0d2edfd00e6e34bca4ea631710887b20ed58
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-01 08:44:57 -0700

    prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_number - encodes a string using rot13
+ *
+ *@n: The number to be modified.
+ *
+ *Return: A pointer to the modified number
+ */
+void print_number(int n)
+{
+	int divisor;
+
+	divisor = 1;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (divisor <= n / 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+		int digit = n / divisor;
+		_putchar('0' + digit);
+		n %= divisor;
+		divisor /= 10;
+	}
+}
