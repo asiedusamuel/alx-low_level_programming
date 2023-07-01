a084b75c8bb52c24b9606ba8d87c95426f816bbc
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-01 07:33:38 -0700

    encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *leet - Capitalizes all words of a string
+ *
+ *@str: The string to be modified.
+ *
+ *Return: A pointer to the modified string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char *ptr = str;
+	char leetChars[], letters[];
+
+	leetChars[] = { '4', '3', '0', '7', '1' };
+
+	letters[] = { 'a', 'e', 'o', 't', 'l' };
+
+	while (*ptr != '\0')
+	{
+		i = 0;
+		while (i < 5)
+		{
+			if (*ptr == letters[i] || *ptr == (letters[i] - ('a' - 'A')))
+			{ 	*ptr = leetChars[i];
+				break;
+			}
+
+			i++;
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
