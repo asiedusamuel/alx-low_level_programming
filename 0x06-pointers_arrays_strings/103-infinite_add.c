f6f59d676b14192db3620c6da24e103f8582e4c4
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-01 11:25:46 -0700

    103-infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,31 +12,45 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int carry, i;
-    int digit1, digit2, sum;
+	int len1, len2, carry, i, digit1, digit2, sum;
 
-    carry = 0;
-    i = 0;
+	len1 = strlen(n1);
+	len2 = strlen(n2);
 
-    while (*n1 != '\0' || *n2 != '\0' || carry)
+	if (len1 > size_r - 1 || len2 > size_r - 1)
 	{
-        if (i >= size_r - 1)
+		return 0;
+	}
+
+	carry = 0;
+	i = size_r - 2;
+
+	r[size_r - 1] = '\0';
+
+	while (i >= 0)
+	{
+		digit1 = (len1 > 0) ? n1[len1 - 1] - '0' : 0;
+		digit2 = (len2 > 0) ? n2[len2 - 1] - '0' : 0;
+
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[i] = sum % 10 + '0';
+
+		i--;
+		len1--;
+		len2--;
+	}
+
+	if (carry)
+	{
+		if (size_r == 1)
 		{
-            return 0;
-        }
+			return 0;
+		}
 
-        digit1 = (*n1 != '\0') ? *n1 - '0' : 0;
-        digit2 = (*n2 != '\0') ? *n2 - '0' : 0;
+		memmove(r + 1, r, size_r - 1);
+		r[0] = carry + '0';
+	}
 
-        sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        r[i] = sum % 10 + '0';
-
-        i++;
-        n1++;
-        n2++;
-    }
-
-    r[i] = '\0';
-    return (r);
+	return r;
 }
