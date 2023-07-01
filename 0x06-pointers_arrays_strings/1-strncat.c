ce52f6a913bf6440213da9c7791df069556e8124
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-01 06:42:09 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strcat - Concatenates the source string to the destination string
+ *
+ *@dest: The destination string to which the source string will be appended.
+ *
+ *@src: The source string that will be appended to the destination string.
+ *
+ *@n: The maximum number of characters to be concatenated from the source string
+ *
+ *Return: A pointer to the destination string.
+ */
+char *_strncat(char *dest, const char *src, int n)
+{
+	int dest_len, i;
+	dest_len = 0;
+	i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
