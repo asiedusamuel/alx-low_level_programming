91fdc1058593910ae69dc5e500da5aa04bbf3df4
SamuelAsiedu <mybusinessconcepts@gmail.com>
2023-07-01 09:43:10 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,5 +9,29 @@
  */
 void print_number(int n)
 {
-	_putchar('0' + n);
+	if (n == 0) {
+        _putchar('0');
+        return;
+    }
+
+    if (n < 0) {
+        _putchar('0' + n);
+        n = -n;
+    }
+
+    int divisor = 1;
+    int temp = n;
+
+    while (temp > 9) {
+        divisor *= 10;
+        temp /= 10;
+    }
+
+    while (divisor > 0) {
+        int digit = n / divisor;
+		
+        _putchar('0' + digit);
+        n %= divisor;
+        divisor /= 10;
+    }
 }
